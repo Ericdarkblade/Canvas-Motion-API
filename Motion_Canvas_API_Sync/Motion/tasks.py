8185ceab6a466bce836efda7a644404a9af3c9fd
Ericdarkblade <ezmoswalker@gmail.com>
2024-02-06 20:23:47 -0500

    working on tasks now

---
+++ Motion_Canvas_API_Sync/Motion/tasks.py
@@ -0,0 +1,46 @@
+import requests
+import Motion_Canvas_API_Sync.Motion.secrets as secrets
+
+secret_api_key = secrets.api_key
+
+# payload["labels"] = ["Imported Assignment"]
+
+
+def create_task(
+    dueDate: str,
+    name: str,
+    workspaceId: str,
+    description: str= None,
+    labels: [str] = None,
+    *,
+    autoScheduled: bool = True,
+    duration="NONE",
+    startDate: str = None,
+    deadlineType: str = "HARD",
+    schedule: str = "Work Hours",
+    status: str = None,
+    api_key: str = secret_api_key
+):
+    url = "https://api.usemotion.com/v1/tasks"
+    headers = {"Accept": "application/json", "X-API-Key": api_key}
+
+    payload = {
+        "dueDate": dueDate,
+        "duration": duration,
+        "autoScheduled": {
+            "startDate": startDate,
+            "deadlineType": deadlineType,
+            "schedule": schedule,
+        },
+        "name": name,
+        "workspaceId": workspaceId,
+        "description": description,
+        "status": status,
+        "labels": labels,
+    }
+    if not autoScheduled:
+        payload["autoScheduled"] = None
+
+    print(payload)
+
+    #motion_response = requests.get(url, headers=headers).json()
--- Motion_Canvas_API_Sync/Motion/workspaces.py
+++ Motion_Canvas_API_Sync/Motion/workspaces.py
@@ -1,13 +1,20 @@
 import requests
-import json
 import Motion_Canvas_API_Sync.Motion.secrets as secrets
 
-
 api_key = secrets.api_key
-target_workspace_name = secrets.workspace_name
+secret_workspace_name = secrets.workspace_name
 
+#Finds the workspace_id of a workspace in motion, provided a workspace name, and API KEY
+def find_target_workspace_id(target_workspace_name: str=secret_workspace_name, *, api_key: str=api_key) -> str:
+    """_summary_
 
-def find_target_workspace_id(target_workspace_name=target_workspace_name):
+    Args:
+        target_workspace_name (str, optional): The Workspace Name you want to identify. Defaults to sercet_workspace_name.
+        api_key (str, optional): The API KEY for usemotion.com. Defaults to secret_api_key.
+
+    Returns:
+        str: Workspace ID
+    """
     url = "https://api.usemotion.com/v1/workspaces"
 
     headers = {"Accept": "application/json", "X-API-Key": api_key}
