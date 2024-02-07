79a870ca1fc439fa00c4b817b8c990fd71940e83
Ericdarkblade <ezmoswalker@gmail.com>
2024-02-07 12:47:05 -0500

    Starting the projects module

---
+++ Motion_Canvas_API_Sync/Motion/projects.py
@@ -0,0 +1,10 @@
+import requests
+from workspaces import Workspace
+import secrets
+
+api_key = secrets.api_key
+
+
+class Project:
+    def __init__(self, workspace: Workspace) -> None:
+        pass
